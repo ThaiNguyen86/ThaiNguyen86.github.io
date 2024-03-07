d3e342a27f2caa245aac24e316a2ce28f27e50e4
ThaiNguyen86 <ngtthai0604@gmail.com>
2024-03-07 20:22:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showGreeting() {
     if (name.trim() !== "") {
       var greeting = "";
       if(name === "Vy") {
-          greeting = "Chúc " + name + " luôn vui vẻ ✨ \n xinh đẹp và thành công trên con đường đã chọn. \n Chúc Vy tất cả 🌸🌸🌸";
+          greeting = "Chúc " + name + " luôn vui vẻ ✨✨✨ \n xinh đẹp và thành công trên con đường đã chọn. \n Chúc Vy tất cả 🌸🌸🌸";
       } else {
           greeting = "Chúc " + name + " 8/3 \n vui vẻ nha !!! 😊";
           
